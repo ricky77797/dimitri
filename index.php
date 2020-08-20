f0231f2e62eefff1361bd47684c77d936f88fb1b
ricky77797 <69971584+ricky77797@users.noreply.github.com>
2020-08-20 09:12:23 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>inicio</title>
+	<link rel="stylesheet" href="styles/styles2.css">
+</head>
+<body>
+	<video autoplay muted loop>
+		<source src="videos/carga.mp4" type="video/mp4">
+		</video>
+				<h1 class = "title"> ASDELL STORE </h1>
+				<section class="portafolio">
+					<div class="portafolio-conteiner">
+
+						<section class="portafolio-item">
+							<a href="videos/asus2.mp4">
+							<img src="imag/asusg.jpg"alt="" class="portafolio-img" >
+							</a>
+							<section class="portafolio-text">
+								<h2>ASUS ZEPHYRUS G14</h2>
+								<p>Dinámico y listo para viajar, el pionero ROG Zephyrus G14 es el portátil para juegos Windows 10 Pro de 14 pulgadas más potente del mundo. Supera a la competencia con una CPU AMD Ryzen ™ 9 4900HS de hasta 8 núcleos y una potente GPU GeForce RTX ™ 2060 que acelera a través de la multitarea y los juegos cotidianos. Personalice su ajuste con una pantalla de juegos de 120Hz o un panel WQHD de alta resolución, ambos Pantone® Validated para una precisión de color excelente. Los altavoces cuádruples producen un increíble sonido Dolby Atmos para películas, juegos, música y más inmersivos. Vive la vida a la velocidad de Zephyrus con una laptop para juegos ligera y portátil, y mantente activo en cualquier lugar.</p>
+							</section>
+						</section>
+						<section class="portafolio-item">
+							<a href="videos/asus1.mp4">
+							<img src="imag/asuss.jpg"alt="" class ="portafolio-img">
+							</a>
+							<section class="portafolio-text">
+								<h2>ASUS ZEPHYRUS S</h2>
+								<p>Después de redefinir las laptops ultradelgadas para juegos con el Zephyrus original, ROG lleva los juegos hardcore de Windows 10 Pro a un diseño aún más delgado enmarcado por biseles súper estrechos. ROG Zephyrus S utiliza ingeniería innovadora para enfriar su GPU GeForce RTXTM 2080 y el procesador Intel® Core ™ i7 de novena generación , para que pueda sumergirse en su pantalla de 240Hz / 3ms sin compromiso con Validación de color Pantone® .</p>
+							</section>
+						</section>
+						<section class="portafolio-item">
+							<a href="videos/razer.mp4">
+							<img src="imag/razer.jpg"alt="" class ="portafolio-img">
+							</a>
+							<section class="portafolio-text">
+								<h2>RAZER BLADE PRO</h2>
+								<p>El Razer Blade 15 con NVIDIA GeForce RTX 2070 con gráficos de diseño Max-Q ofrece la mejor experiencia de juego para PC. Alimentado por un procesador Intel Core i7 6 core de 8a generación y con una pantalla Full HD de 15.6 "y 144Hz, alojado en un chasis de aluminio CNC compacto y de precisión diseñado con solo 0.70" de espesor.
+								</p>
+							</section>
+						</section>
+					</div>
+				</section>
+	<a class = "b" href="inc.php">iniciar sesión</a>
+	<div class = "sidebar">
+	<a href="slider/slider.html">	
+	<img class ="imagen" src="imag/slas.jpg">
+	</a>
+	</div>
+</body>
+</htm>
